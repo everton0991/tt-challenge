253becf7c5d95eff1a4f9b69ff92a82a79f2d311
Everton Renan <everton.renan@clevertech.biz>
2025-01-25 10:23:59 -0300

    feat: improve result message

--- src/components/PaymentDashboard.tsx
+++ src/components/PaymentDashboard.tsx
@@ -8,6 +8,11 @@ import TransactionsList, {
 import CheckTransactionForm from './paymentDashboard/CheckTransactionForm';
 import AddNewTransactionForm from './paymentDashboard/AddNewTransactionForm';
 
+interface ResultMessage {
+  error: boolean;
+  message: string;
+}
+
 const PaymentDashboard: React.FC = () => {
   const [transactions, setTransactions] = useState<Array<Transaction>>([
     { id: 1, amount: 50 },
@@ -20,18 +25,21 @@ const PaymentDashboard: React.FC = () => {
   const [transactionAmount, setTransactionAmount] = useState<
     number | undefined
   >(0);
-  const [result, setResultMessage] = useState<string>('');
+  const [resultMessage, setMessage] = useState<ResultMessage>({
+    error: false,
+    message: '',
+  });
 
   const handleCheckTransactions = () => {
-    setResultMessage('');
+    setMessage({ error: false, message: '' });
 
     if (target === undefined || typeof target !== 'number') {
-      setResultMessage(ERROR_MESSAGES.INVALID_TARGET);
+      setMessage({ error: true, message: ERROR_MESSAGES.INVALID_TARGET });
       return;
     }
 
     if (target < 0) {
-      setResultMessage(ERROR_MESSAGES.NEGATIVE_TARGET);
+      setMessage({ error: true, message: ERROR_MESSAGES.NEGATIVE_TARGET });
       return;
     }
 
@@ -46,70 +54,87 @@ const PaymentDashboard: React.FC = () => {
         );
 
         if (matchingTransaction) {
-          setResultMessage(
-            `Transactions ${matchingTransaction.id} and ${transaction.id} add up to ${target}`
-          );
+          setMessage({
+            error: false,
+            message: `Transactions ${matchingTransaction.id} and ${transaction.id} add up to ${target}`,
+          });
           return;
         }
       }
       amountsSet.add(transaction.amount);
     }
 
-    setResultMessage(ERROR_MESSAGES.NO_MATCHING_TRANSACTIONS);
+    setMessage({
+      error: true,
+      message: ERROR_MESSAGES.NO_MATCHING_TRANSACTIONS,
+    });
   };
 
   const handleAddTransaction = (id?: number, amount?: number) => {
     if (id === undefined) {
-      setResultMessage(ERROR_MESSAGES.NULL_ID);
+      setMessage({ error: true, message: ERROR_MESSAGES.NULL_ID });
       return;
     }
 
     if (amount === undefined || amount < 0) {
-      setResultMessage(ERROR_MESSAGES.NULL_OR_NEGATIVE_AMOUNT);
+      setMessage({
+        error: true,
+        message: ERROR_MESSAGES.NULL_OR_NEGATIVE_AMOUNT,
+      });
       return;
     }
 
     if (transactions.some((transaction) => transaction.id === id)) {
-      setResultMessage(ERROR_MESSAGES.ID_EXISTS(id));
+      setMessage({ error: true, message: ERROR_MESSAGES.ID_EXISTS(id) });
       return;
     }
 
     setTransactions([...transactions, { id, amount }]);
-    // Reset input fields after adding
     setTransactionId(undefined);
     setTransactionAmount(undefined);
   };
 
   return (
-    <div className='max-w-2xl mx-auto p-6 bg-gray-800 shadow-lg rounded-lg'>
-      <h1 className='text-2xl font-bold mb-4 text-white text-center'>
+    <div className='max-w-5xl mx-auto p-6 bg-gray-800 shadow-lg rounded-lg'>
+      <h1 className='text-2xl font-bold mb-8 text-white text-center'>
         Payment Transaction Dashboard
       </h1>
 
-      <TransactionsList transactions={transactions} />
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+        <div className='col-span-1'>
+          <TransactionsList transactions={transactions} />
+        </div>
 
-      {/* Divider */}
-      <hr className='border-gray-600 mb-4' />
-      <CheckTransactionForm
-        target={target}
-        setTarget={setTarget}
-        handleCheckTransactions={handleCheckTransactions}
-      />
+        <div className='col-span-1'>
+          <div className='mb-4'>
+            <CheckTransactionForm
+              target={target}
+              setTarget={setTarget}
+              handleCheckTransactions={handleCheckTransactions}
+            />
+          </div>
 
-      {/* Divider */}
-      <hr className='border-gray-600 mb-4' />
-      <AddNewTransactionForm
-        transactionId={transactionId}
-        transactionAmount={transactionAmount}
-        setTransactionId={setTransactionId}
-        setTransactionAmount={setTransactionAmount}
-        handleAddTransaction={handleAddTransaction}
-      />
+          <hr className='border-gray-600 mb-4' />
 
-      {/* Result Message */}
-      {result && (
-        <div className='mt-4 text-center text-red-400'>
-          <p>{result}</p>
+          <div className='mb-4'>
+            <AddNewTransactionForm
+              transactionId={transactionId}
+              transactionAmount={transactionAmount}
+              setTransactionId={setTransactionId}
+              setTransactionAmount={setTransactionAmount}
+              handleAddTransaction={handleAddTransaction}
+            />
+          </div>
+        </div>
+      </div>
+
+      {resultMessage.message && (
+        <div
+          className={`mt-4 text-center ${
+            resultMessage.error ? 'text-red-400' : 'text-green-400'
+          }`}
+        >
+          <p>{resultMessage.message}</p>
         </div>
       )}
     </div>
